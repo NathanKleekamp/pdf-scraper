60f2c7b83569cd715b88ef2cd3a1010a03b2dc52
Nathan Kleekamp <nkleekamp@gmail.com>
2012-06-25 21:27:04 -0400

    refactoring to support j1

--- scraper.py
+++ scraper.py
@@ -1,38 +1,25 @@
 #!/usr/bin/env python
 
 '''
-Scrapes exchanges for pdf files. Returns a csv file with each pdf in a given
-section and the page(s) on which they're linked.
+Spiders a site or section of a site looking for pdfs. By default, this
+script will not spider links offsite.
 '''
-from __future__ import print_function
 
+from __future__ import print_function
 
 import re
 import csv
 import sys
+import urlparse
 import lxml
+import domains
 import requests
-
 
 from bs4 import BeautifulSoup
 from sqlalchemy import create_engine, Column, Integer, String, Boolean,\
      ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship, backref
-
-
-# Global Variables
-try:
-    start = sys.argv[1]
-except IndexError:
-    print('You must enter a starting point, like http://exchanges.state.gov/heritage/index.html')
-    start = raw_input('Enter a starting point: ')
-
-parsed = start.split('/')
-baseurl = '/'.join(parsed[:3])
-path = '/'.join(parsed[3:len(parsed)])
-directory = parsed[3]
-log = open('log.txt', 'a')
 
 
 # Setting up the database and database classes
@@ -79,80 +66,58 @@ class PageUrl(Base):
 Base.metadata.create_all(engine)
 
 
-# Functions
-def visited(address):
-    address = address.split(baseurl)[1]
+class Url(object):
+
+    def __init__(self, url):
+        self.url = url
+        self.base = 'http://{0}'.format(urlparse.urlparse(url).netloc)
+        self.path = urlparse.urlparse(url).path
+
+
+try:
+    start = Url(sys.argv[1])
+except IndexError:
+    print('You must enter a starting point, like http://exchanges.state.gov/heritage/index.html')
+    start = Url(raw_input('Enter a starting point: '))
+
+
+def get_pdfs(soup, address):
+    unique_pdfs = set([link.get('href') for link in soup.find_all(
+                      'a', href=re.compile('\.pdf'))])
+    for pdf in unique_pdfs:
+        if not session.query(Pdf).filter(
+                Pdf.url==urlparse.urljoin(start.base, pdf)).first():
+            pdf = Pdf(urlparse.urljoin(start.base, pdf))
+            pdf.page_urls.append(PageUrl(address))
+            session.add(pdf)
+        else:
+            pdf = session.query(Pdf).filter(
+                  Pdf.url==urlparse.urljoin(start.base, pdf)).first()
+            pdf.page_urls.append(PageUrl(address))
+            session.add(pdf)
+    session.commit()
+
+
+def visited(soup, address):
+    # This will not work on J1 because it's looking only for a path in the db
+    address = urlparse.urlparse(address).path
     not_visited = session.query(SpiderUrl).filter(
-                    SpiderUrl.url==address).first
+                  SpiderUrl.url==address).first
     if not_visited() is not None:
         url = not_visited()
         url.visited = True
 
 
-# This function will create duplicate copies on the page_urls table if the
-# script is run on the same page more than once. Needs fixin'
-def processer(soup, address):
-    '''
-    Spiders pages and looks for pdfs. Records on which page the pdf is
-    located.
-    '''
-    spider_urls = soup.find_all('a', href=re.compile("\.html$"))
-    unique_links = set([link.get('href') for link in spider_urls])
-    pdf_links = soup.find_all('a', href=re.compile("\.pdf$"))
-    unique_pdfs = set([link.get('href') for link in pdf_links])
-    for link in unique_links:
-        if 'cms' in link or 'staging' in link:
-            message = 'Found link to CMS or staging ({0}) on: {1}\n'.format(
-                       link, address)
-            log.write(message)
-            print(message, end='')
-            pass
-        elif directory in link and baseurl in link:
-            message = 'Found non-relative link: {0} on: {1}\n'.format(
-                        link, address)
-            log.write(message)
-            print(message, end='')
-            pass
-        # This should ignore outside links with directory name
-        elif directory in link and 'http://' in link and basurl not in link:
-            pass
-        elif directory in link:
-            if not session.query(SpiderUrl).filter(
-                   SpiderUrl.url==link).all():
-                message = 'Adding {0} to DB\n'.format(link)
-                log.write(message)
-                print(message, end='')
-                url = SpiderUrl(link)
-                session.add(url)
-    for pdf in unique_pdfs:
-        if not session.query(Pdf).filter(Pdf.url==pdf).all():
-            pdf = Pdf(pdf)
-            pdf.page_urls.append(PageUrl(address))
-            session.add(pdf)
-        else:
-            pdf = session.query(Pdf).filter(
-                  Pdf.url==pdf).all()
-            pdf[0].page_urls.append(PageUrl(address))
-            session.add(pdf[0])
-    visited(address)
-    session.commit()
-
-
 def main():
-    r = requests.get(start)
+    r = requests.get(start.url)
     while True:
         soup = BeautifulSoup(r.text, 'lxml')
-        message = 'Checking {0}\n'.format(r.url)
-        print(message, end='')
-        log.write(message)
-        processer(soup, r.url)
+        get_pdfs(soup, r.url)
         not_visited = session.query(SpiderUrl).filter(
-                        SpiderUrl.visited==False).first
+                      SpiderUrl.visited==False).first
         if not not_visited():
             break
-        # This needs to be fixed. Should only check if not visited is a
-        # relative url.
-        r = requests.get(baseurl+not_visited().url)
+        r.requests.get(urlparse.urljoin(start.base, not_visited().url))
         if r.status_code == 404:
             pass
     with open('output.csv', 'wb') as f:
@@ -162,6 +127,7 @@ def main():
             writer.writerow([record.url])
             for entry in record.page_urls:
                 writer.writerow(['', entry.url])
+
 
 if __name__ == '__main__':
     main()
